3b8247580c7ec21e3681565f26bee60f807cdf5f
pechkul.yuenyong <pechkul.yuenyong@gmail.com>
2024-06-21 13:58:18 +0700

    add docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import appConfig from './config/app.config';
-import { UserEntity } from './users/entities/user.entity';
-import { EventEntity } from './users/entities/event.entity';
 
 @Module({
   imports: [
@@ -14,24 +11,6 @@ import { EventEntity } from './users/entities/event.entity';
       envFilePath: ['.env', '.env.development', '.env.test', '.env.production'],
       load: [appConfig],
     }),
-    TypeOrmModule.forFeature(
-      [UserEntity],
-      {
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        database: 'nestjs',
-        username: 'root',
-        password: '',
-        synchronize: true,
-      }),
-    TypeOrmModule.forFeature(
-      [EventEntity],
-      {
-        type: 'sqlite',
-        database: 'nestjs.sqlite',
-        synchronize: true,
-      }),
     UsersModule,
   ],
   controllers: [AppController],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,7 +6,19 @@ import { UserEntity } from './entities/user.entity';
 import { EventEntity } from './entities/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, EventEntity])],
+  imports: [
+    TypeOrmModule.forFeature(
+      [UserEntity, EventEntity],
+      {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        database: 'nestjs-cqrs',
+        username: 'root',
+        password: 'root',
+        synchronize: true,
+      }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
